749824da0e7a0da57a28bfa5891d5c4e3314eba1
AchmadZahrulAZ <achmadzahrul01@gmail.com>
2023-10-24 15:02:20 +0700

    Update Main menu & Penutup Scene
    Fixing Play, Exit Button on Mainmenu scene.
    
    Fixing NextSceneLoader on Penutup scene

---
+++ Assets/Scripts/Game/Mainmenu/mainmenu_manager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Intro", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
