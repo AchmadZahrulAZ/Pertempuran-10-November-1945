4af98b085d245f7d22ef339ddd741db93d062ae2
AchmadZahrulAZ <achmadzahrul01@gmail.com>
2023-11-13 21:24:42 +0700

    Adding Continue Game Purpose
    Add and changing some script for continue game purpose

--- Assets/Scripts/Game/Mainmenu/mainmenu_manager.cs
+++ Assets/Scripts/Game/Mainmenu/mainmenu_manager.cs
@@ -2,12 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Mainmenu : MonoBehaviour
 {
+    [Header("Menu Buttons")]
+
+    [SerializeField] private Button newGameButton;
+
+    [SerializeField] private Button continueGameButton;
+
+    private void Start()
+    {
+        if (!DataPersistenceManager.instance.HasGameData())
+        {
+            continueGameButton.interactable = false;
+        }
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("Intro", LoadSceneMode.Single);
+        DisableMenuButtons();
+        // create a new game - which will initialize our game data
+        DataPersistenceManager.instance.NewGame();
+        // Load the gameplay scene - which will in turn save the game because of
+        // OnSceneUnloaded() in the DataPersistenceManager
+        SceneManager.LoadSceneAsync("Intro");
+        //SceneManager.LoadScene("Intro", LoadSceneMode.Single);
+    }
+
+    public void ContinueGame()
+    {
+        DisableMenuButtons();
+        // Load the gameplay scene - which will in turn save the game because of
+        // OnSceneUnloaded() in the DataPersistenceManager
+        SceneManager.LoadSceneAsync("Intro");
     }
 
     public void TutorialGame()
@@ -18,5 +47,11 @@ public class Mainmenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void DisableMenuButtons()
+    {
+        newGameButton.interactable = false;
+        continueGameButton.interactable = false;
     }
 }
